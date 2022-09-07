9a7b13b0ba88d2a4ac25636f836676dc2ed7f6ee
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2022-09-07 19:09:30 +0300

    - add exercise with regular expressions v.15 in code.php

--- .idea/classes/code.php
+++ .idea/classes/code.php
@@ -1242,4 +1242,15 @@ $str = 'aba aea aca aza axa a.a a+a a*a';
 $r = preg_replace('#a[.+*]a#','!',$str);
 echo $r;
 echo '<br>' . "***" . '</br>';
+
+/*
+ * <?php
+	$str = 'xaz x.z x3z x@z x$z xrz';
+?>
+Напишите регулярку, которая найдет строки по шаблону: буква 'x', затем НЕ точка, НЕ собака, и НЕ доллар, а потом буква 'z'.
+ */
+$str = 'xaz x.z x3z x@z x$z xrz';
+$r = preg_replace('#x[^.@$]z#', '!', $str);
+echo $r;
+echo '<br>' . "***" . '</br>';
 ?>
